4980cd44b2d2e828c7a0294997b10d50f7df807f
Fares Krouma <fkrouma@nettask.de>
2024-11-20 09:12:10 +0100

    - init gluestack library

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,11 @@
-import { Text, View } from "react-native";
+
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function Index() {
+  
 	return (
-		<View
-			style={{
-				flex: 1,
-				justifyContent: "center",
-				alignItems: "center",
-			}}
-		>
-			<Text>Edit app/index.tsx to edit this screen.</Text>
-		</View>
-	);
+   <Text>Hello World!</Text>
+  );
+  ;
 }
+
