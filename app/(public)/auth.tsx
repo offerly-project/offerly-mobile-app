dfec4673b47db3b2174584efecd559b5a6c6bad1
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-05 22:33:31 +0400

    dark-light brand logo in auth

--- app/(public)/auth.tsx
+++ app/(public)/auth.tsx
@@ -1,7 +1,9 @@
-import BrandLogo from '@/assets/images/BrandLogo.svg';
+import BrandLogoDark from '@/assets/images/offerly-logo-dark.svg';
+import BrandLogoLight from '@/assets/images/offerly-logo-light.svg';
 import Button from '@/components/Button/Buttton';
 import Link from '@/components/Typography/Link';
 import Typography from '@/components/Typography/Typography';
+import { useThemeContext } from '@/contexts/ThemeContext';
 import { useThemeStyles } from '@/hooks/useThemeStyles';
 import { languageStore } from '@/stores';
 import { router } from 'expo-router';
@@ -10,11 +12,13 @@ import { SafeAreaView, View } from 'react-native';
 export default function index() {
 	const theme = useThemeStyles();
 	const { translations } = languageStore();
+	const themeName = useThemeContext().theme;
+	const Logo = themeName === 'dark' ? BrandLogoDark : BrandLogoLight;
 	return (
 		<SafeAreaView className='flex-1 bg-background'>
 			<View className='flex-1 px-8 justify-center items-center'>
 				<View className='gap-5 w-full'>
-					<BrandLogo height={200} width={200} style={{ margin: 'auto' }} />
+					<Logo height={200} width={200} style={{ margin: 'auto' }} />
 					<Button onPress={() => router.push('/login')}>
 						<Typography color={theme['--primary']}>
 							{translations.buttons.login}
