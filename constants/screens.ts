339f58fcb72396e5d184145408ac20aaee2dbeee
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-12 13:07:13 +0400

    Revert "Revert "rtl routes""
    This reverts commit b864c1da341c265b90fed220bb21266a8c4e45a3.

--- constants/screens.ts
+++ constants/screens.ts
@@ -1,11 +1,14 @@
+import { languageStore } from '@/stores';
 import { Dimensions } from 'react-native';
 import { ThemeStyle } from './themes';
 
 export const getBaseScreenLayout = (theme: ThemeStyle): object => {
+	const animation = languageStore().language === 'ar' ? 'slide_from_left' : 'slide_from_right';
 	return {
 		backgroundColor: theme['--background'],
 		headerShown: false,
 		flex: 1,
+		animation,
 		contentStyle: {
 			backgroundColor: theme['--background'],
 		},
@@ -13,10 +16,12 @@ export const getBaseScreenLayout = (theme: ThemeStyle): object => {
 };
 
 export const getAuthScreenLayout = (theme: ThemeStyle): object => {
+	const animation = languageStore().language === 'ar' ? 'slide_from_left' : 'slide_from_right';
 	return {
 		headerBackVisible: true,
 		headerTransparent: true,
 		headerTitle: '',
+		animation,
 		headerTintColor: theme['--primary'],
 		headerShown: false,
 	};
