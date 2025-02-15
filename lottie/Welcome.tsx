1e38d725f58e42a0a223a83ca3930cc838037e2b
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-02-15 20:22:42 +0400

    gradient splash

--- lottie/Welcome.tsx
+++ lottie/Welcome.tsx
@@ -1,5 +1,5 @@
-import { themes } from '@/constants/themes';
 import { useIsFocused } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
 import LottieView from 'lottie-react-native';
 import { useEffect, useRef } from 'react';
 import { StyleSheet } from 'react-native';
@@ -16,23 +16,35 @@ const Welcome = ({ onFinish }: Props) => {
 		}
 	}, [focused]);
 	return (
-		<LottieView
-			ref={ref}
-			onAnimationFinish={onFinish}
-			source={require('./animations/welcome-animation.json')}
-			autoPlay
-			loop={false}
-			speed={1.6}
-			style={{
-				width: '100%',
-				height: '100%',
-				padding: '10%',
-				backgroundColor: themes.light['--primary'],
-			}}
-		/>
+		<LinearGradient
+			colors={['#6633cc', '#000000']}
+			start={{ x: 1, y: 0 }}
+			end={{ x: 0, y: 1 }}
+			style={styles.gradient}
+		>
+			<LottieView
+				ref={ref}
+				onAnimationFinish={onFinish}
+				source={require('./animations/welcome-animation.json')}
+				autoPlay
+				loop={false}
+				speed={1.6}
+				style={{
+					width: '100%',
+					height: '100%',
+					padding: '10%',
+				}}
+			/>
+		</LinearGradient>
 	);
 };
 
 export default Welcome;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	gradient: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+});
